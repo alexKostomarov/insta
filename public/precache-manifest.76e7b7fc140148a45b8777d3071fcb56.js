88dec6fe51dc3cf825ccf982f61d15bcce5bde83
Alexander <alex@w-online.ru>
2020-04-02 19:49:07 +0300

    config js

---
+++ public/precache-manifest.76e7b7fc140148a45b8777d3071fcb56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fea11f8035802f29d1538524904e86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef55f6460f1db68a6f6",
+    "url": "/static/css/main.2de84d49.chunk.css"
+  },
+  {
+    "revision": "56203d8f1b880c46e2b0",
+    "url": "/static/js/2.6fe0ce60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6fe0ce60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef55f6460f1db68a6f6",
+    "url": "/static/js/main.648a25a6.chunk.js"
+  },
+  {
+    "revision": "13aa65976f3d2cf28a82",
+    "url": "/static/js/runtime-main.e8d1df9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
